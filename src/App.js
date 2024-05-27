620ffd953fc0b38e6f559169991e9c78187287f2
MoizAnsari7 <moizansari176@gmail.com>
2024-05-27 18:12:36 +0530

    Question: add new question under test
    Component to add new question is added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import CreateAssessmentComponent from "./components/assessment.component";
 import AssessmentList from "./components/assessment-list.component";
 import CreateTestComponent from "./components/test.component";
 import TestCompoent from "./components/test-list.component"
+import CreateQuestionComponent from "./components/question.component";
 
 function App() {
   return (
     <>
         {/* <CreateAssessmentComponent/>
         <AssessmentList/> */}
-        <CreateTestComponent/>
-        <TestCompoent/>
+        {/* <CreateTestComponent/>
+        <TestCompoent/> */}
+        <CreateQuestionComponent/>
     </>
   );
 }
