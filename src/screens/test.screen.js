4365f952254ed37c4f7179b0845de9e702c54000
MoizAnsari7 <moizansari176@gmail.com>
2024-06-01 10:40:15 +0530

    Question and Answer panel
    Question and Answer panel
    Question panel :
    1. question number and total question with button for flag and info in header box
    2. question text
    
    Answer panel
    1. Answer header as choose the correction option
    2. option with checkbox and radio button
    3. clear response and next question button

--- src/screens/test.screen.js
+++ src/screens/test.screen.js
@@ -1,9 +1,11 @@
+import QuestionPanelComponent from "../components/question-panel.component";
 import TestHeaderComponent from "../components/test-header.component";
 
 const  TestScreen = ()=>{
     return (
         <>
         <TestHeaderComponent/>
+        <QuestionPanelComponent/>
         </>
     )
 }
