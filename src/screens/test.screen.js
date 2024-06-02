d7afc2eb17268bbd1f9849edc0914a46fd095c40
MoizAnsari7 <moizansari176@gmail.com>
2024-06-02 22:51:47 +0530

    test screen added

--- src/screens/test.screen.js
+++ src/screens/test.screen.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import QuestionPanelComponent from "../components/question-panel.component";
+import TestFooterComponent from "../components/test-footer.component";
 import TestHeaderComponent from "../components/test-header.component";
+import './test.screen.css';
 
-const  TestScreen = ()=>{
-    return (
-        <>
-        <TestHeaderComponent/>
-        <QuestionPanelComponent/>
-        </>
-    )
+const TestScreen = () => {
+  return (
+    <div className="test-screen-container">
+      <TestHeaderComponent />
+      <div className="content-container">
+        <QuestionPanelComponent />
+      </div>
+      <TestFooterComponent />
+    </div>
+  );
 }
 
 export default TestScreen;
