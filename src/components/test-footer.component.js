b3c591c3bcb97b922ce93a8213dc1dc796e7066c
MoizAnsari7 <moizansari176@gmail.com>
2024-06-02 22:49:24 +0530

    TEST footer component added
    TEST footer component added
    with submit section button and submit test button .
    also user name and test title is also shown

--- src/components/test-footer.component.js
+++ src/components/test-footer.component.js
@@ -1,9 +1,14 @@
-const  = ()=>{
+import "./test-footer.component.css"
+const TestFooterComponent = ()=>{
     return (
-        <>
-
-        </>
-    )
+    <footer className="footer">
+      <div className="left-text">
+        <span className="footer-text">Some left text 1</span>
+        <span className="footer-text">Some left text 2</span>
+      </div>
+      <button className="finish-test-button">Finish Test</button>
+    </footer>
+  );
 }
 
-export default ;
+export default TestFooterComponent;
