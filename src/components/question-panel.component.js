bfa9040d297c16c543049be4a44d1bbbc2259905
MoizAnsari7 <moizansari176@gmail.com>
2024-06-02 22:53:52 +0530

    question side panel , question with option component
    question side panel to show question list,
    question with option component in question dashboard

--- src/components/question-panel.component.js
+++ src/components/question-panel.component.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './QuestionPanelComponent.css';
+<<<<<<< Updated upstream
 import OptionPanelComponent from './option-panel.component';
 import QuestionComponent from './question-box';
+=======
+import QuestionPanelListComponent from './QuestionPanelList.component';
+>>>>>>> Stashed changes
 
 const QuestionPanelComponent = () => {
     return (
         <div className="question-panel">
+            <QuestionPanelListComponent/>
             {/* Left Section */}
             <QuestionComponent/>        
             <div className="divider"></div>
