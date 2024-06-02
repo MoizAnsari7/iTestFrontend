6faf02ed34397b0407e3df59d6da1deb794bf2b3
MoizAnsari7 <moizansari176@gmail.com>
2024-06-02 22:58:13 +0530

    some information added

--- src/components/test-footer.component.js
+++ src/components/test-footer.component.js
@@ -3,10 +3,11 @@ const TestFooterComponent = ()=>{
     return (
     <footer className="footer">
       <div className="left-text">
-        <span className="footer-text">Some left text 1</span>
-        <span className="footer-text">Some left text 2</span>
+        <span className="footer-text">Angular Test &nbps; | &nbsp;</span>
+        <span className="footer-text">ansari moiz ( user@gmail.com )</span>
       </div>
       <button className="finish-test-button">Finish Test</button>
+      <button className="finish-test-button">Submit Assessment</button>
     </footer>
   );
 }
