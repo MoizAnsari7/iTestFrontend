119f584661e92c8600399c870af3ca26a9af7dfd
MoizAnsari7 <moizansari176@gmail.com>
2024-06-03 19:34:35 +0530

    Show question list into side panel
    Show question list into side panel
    highlight current question in the list

--- src/components/QuestionPanelList.component.js
+++ src/components/QuestionPanelList.component.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './QuestionPanelListComponent.css';
 import axios from 'axios';
-const QuestionPanelListComponent = (props) => {
-    const [ currentIndex , setCurrentIndex ] = useState(0);
-    const [ questionList , setQuestionList ] = useState([]);
-    useEffect(() => {
-        const fetchQuestionList = async () => {
-          try {
-            //through test id we will get question and its options
-            const response = await axios.get('http://localhost:5000/v1/question/questionWithAnswer/664f5fa79cadea9bd09f44ed');
-            console.log(response.data.data);
-            setQuestionList(response.data.data)
-          } catch (error) {
-            console.error('Error fetching the question with options for a test:', error);
-          }}
-          fetchQuestionList()
-    },[])
+const QuestionPanelListComponent = ({ questionList, currentQuestionIndex, changeCurrentQuestionIndex }) => {
+    // const [ currentIndex , setCurrentIndex ] = useState(0);
+    // const [ questionList , setQuestionList ] = useState([]);
+    // useEffect(() => {
+    //     const fetchQuestionList = async () => {
+    //       try {
+    //         //through test id we will get question and its options
+    //         const response = await axios.get('http://localhost:5000/v1/question/questionWithAnswer/664f5fa79cadea9bd09f44ed');
+    //         console.log(response.data.data);
+    //         setQuestionList(response.data.data)
+    //       } catch (error) {
+    //         console.error('Error fetching the question with options for a test:', error);
+    //       }}
+    //       fetchQuestionList()
+    // },[])
     return (
         <div className="side-panel">
             {/* Header with circles */}
@@ -42,7 +42,7 @@ const QuestionPanelListComponent = (props) => {
             <div className="side-panel-list">
                 {
                     (questionList && questionList.length>  0) && questionList.map((question, index)=>{
-                        return (<div  key={question._id} className={ `list-item answered-border ${ ( currentIndex === index ) ? "active" : "" }`}>
+                        return (<div  key={question._id} className={ `list-item answered-border ${ ( currentQuestionIndex === index ) ? "active" : "" }`}>
                             <b>{ index + 1  }. &nbsp;</b>
                             <i>{question.question}</i>
                             </div>)
